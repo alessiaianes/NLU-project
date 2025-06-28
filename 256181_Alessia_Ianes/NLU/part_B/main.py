1cd8e5d7896d49b9a126004aed13a027ca84a194
alessiaianes <ianesalessia2@gmail.com>
2025-06-28 18:26:51 +0200

    Refactor ModelIAS to BertModelIAS; integrate BERT for encoding and update training loops to use attention masks.

--- 256181_Alessia_Ianes/NLU/part_B/main.py
+++ 256181_Alessia_Ianes/NLU/part_B/main.py
@@ -1,7 +1,3 @@
-# This file is used to run your functions and print the results
-# Please write your fuctions or classes in the functions.py
-
-# Import everything from functions.py file
 from functions import *
 from utils import *
 from model import *
@@ -14,33 +10,22 @@ from torch.utils.data import DataLoader
 import torch.optim as optim
 import matplotlib.pyplot as plt
 from tqdm import tqdm
-
-
+import pandas as pd
 
 if __name__ == "__main__":
-    #Wrtite the code to load the datasets and to run your functions
-    # Print the results
-    # Global variables
-    
-
-    # First we get the 10% of the training set, then we compute the percentage of these examples 
-
     portion = 0.10
-
-    intents = [x['intent'] for x in tmp_train_raw] # We stratify on intents
+    intents = [x['intent'] for x in tmp_train_raw]
     count_y = Counter(intents)
-
     labels = []
     inputs = []
     mini_train = []
-
     for id_y, y in enumerate(intents):
-        if count_y[y] > 1: # If some intents occurs only once, we put them in training
+        if count_y[y] > 1:
             inputs.append(tmp_train_raw[id_y])
             labels.append(y)
         else:
             mini_train.append(tmp_train_raw[id_y])
-    # Random Stratify
+
     X_train, X_dev, y_train, y_dev = train_test_split(inputs, labels, test_size=portion, 
                                                         random_state=42, 
                                                         shuffle=True,
@@ -48,113 +33,141 @@ if __name__ == "__main__":
     X_train.extend(mini_train)
     train_raw = X_train
     dev_raw = X_dev
-
     y_test = [x['intent'] for x in test_raw]
 
-    # Intent distributions
-    print('Train:')
-    pprint({k:round(v/len(y_train),3)*100 for k, v in sorted(Counter(y_train).items())})
-    print('Dev:'), 
-    pprint({k:round(v/len(y_dev),3)*100 for k, v in sorted(Counter(y_dev).items())})
-    print('Test:') 
-    pprint({k:round(v/len(y_test),3)*100 for k, v in sorted(Counter(y_test).items())})
-    print('='*89)
-    # Dataset size
-    print('TRAIN size:', len(train_raw))
-    print('DEV size:', len(dev_raw))
-    print('TEST size:', len(test_raw))
-
-
-    words = sum([x['utterance'].split() for x in train_raw], []) # No set() since we want to compute 
-                                                            # the cutoff
-    corpus = train_raw + dev_raw + test_raw # We do not wat unk labels, 
-                                            # however this depends on the research purpose
+    words = sum([x['utterance'].split() for x in train_raw], [])
+    corpus = train_raw + dev_raw + test_raw
     slots = set(sum([line['slots'].split() for line in corpus],[]))
     intents = set([line['intent'] for line in corpus])
-
     lang = Lang(words, intents, slots, cutoff=0)
 
-    # Create our datasets
-    train_dataset = IntentsAndSlots(train_raw, lang)
-    dev_dataset = IntentsAndSlots(dev_raw, lang)
-    test_dataset = IntentsAndSlots(test_raw, lang)
+    train_dataset = IntentsAndSlotsBERT(train_raw, lang)
+    dev_dataset = IntentsAndSlotsBERT(dev_raw, lang)
+    test_dataset = IntentsAndSlotsBERT(test_raw, lang)
 
-
-    # Dataloader instantiations
-    train_loader = DataLoader(train_dataset, batch_size=128, collate_fn=collate_fn,  shuffle=True)
-    dev_loader = DataLoader(dev_dataset, batch_size=64, collate_fn=collate_fn)
-    test_loader = DataLoader(test_dataset, batch_size=64, collate_fn=collate_fn)
-
-    hid_size = 200
+    hid_size_values = [300, 200]
     emb_size = 300
-
-    lr = 0.0001 # learning rate
-    clip = 5 # Clip the gradient
-
+    batch_size_values = [128, 64, 32]
+    dropout_values = [0.1, 0.2, 0.3, 0.4]
+    lr_values = [0.0005, 0.0007, 0.0009,]
+    clip = 5
     out_slot = len(lang.slot2id)
     out_int = len(lang.intent2id)
-    vocab_len = len(lang.word2id)
+    #vocab_len = len(lang.word2id)
 
-    model = ModelIAS(hid_size, out_slot, out_int, emb_size, vocab_len, pad_index=PAD_TOKEN).to(device)
-    model.apply(init_weights)
+    os.makedirs('results/BERT_drop/plots', exist_ok=True)
+    all_results = []
+    total_configurations = len(lr_values) * len(hid_size_values) * len(batch_size_values) * len(dropout_values)
+    current_configuration = 0
 
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
-    criterion_intents = nn.CrossEntropyLoss() # Because we do not have the pad token
+    for hid_size in hid_size_values:
+        for bs in batch_size_values:
+            for d in dropout_values:
+                for lr in lr_values:
+                    print("=" * 89)
+                    print(f"Starting run #{current_configuration + 1} of {total_configurations}")
+                    print("=" * 89)
+                    print(f"Running configuration: lr={lr}, Hid Size={hid_size}, Batch Size={bs}, Dropout={d}")
 
+                    train_loader = DataLoader(train_dataset, batch_size=bs, collate_fn=collate_fn_bert, shuffle=True)
+                    dev_loader = DataLoader(dev_dataset, batch_size=bs//2, collate_fn=collate_fn_bert)
+                    test_loader = DataLoader(test_dataset, batch_size=bs//2, collate_fn=collate_fn_bert)
 
-    n_epochs = 200
-    patience = 3
-    losses_train = []
-    losses_dev = []
-    sampled_epochs = []
-    best_f1 = 0
-    for x in tqdm(range(1,n_epochs)):
-        loss = train_loop(train_loader, optimizer, criterion_slots, 
-                        criterion_intents, model, clip=clip)
-        if x % 5 == 0: # We check the performance every 5 epochs
-            sampled_epochs.append(x)
-            losses_train.append(np.asarray(loss).mean())
-            results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
-                                                        criterion_intents, model, lang)
-            losses_dev.append(np.asarray(loss_dev).mean())
-            
-            f1 = results_dev['total']['f']
-            # For decreasing the patience you can also use the average between slot f1 and intent accuracy
-            if f1 > best_f1:
-                best_f1 = f1
-                # Here you should save the model
-                patience = 3
-            else:
-                patience -= 1
-            if patience <= 0: # Early stopping with patience
-                break # Not nice but it keeps the code clean
+                    model = BertModelIAS(hid_size, out_slot, out_int, dropout=d).to(device)
+                    optimizer = optim.Adam(model.parameters(), lr=lr)
+                    criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
+                    criterion_intents = nn.CrossEntropyLoss()
 
-    results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
-                                            criterion_intents, model, lang)    
-    print('Slot F1: ', results_test['total']['f'])
-    print('Intent Accuracy:', intent_test['accuracy'])
+                    n_epochs = 200
+                    patience = 3
+                    losses_train = []
+                    losses_dev = []
+                    sampled_epochs = []
+                    f1 = []
+                    accuracy = []
+                    best_f1 = 0
 
+                    for x in tqdm(range(1, n_epochs)):
+                        loss = train_loop(train_loader, optimizer, criterion_slots, criterion_intents, model, clip=clip)
+                        if x % 5 == 0:  # We check the performance every 5 epochs
+                            sampled_epochs.append(x)
+                            losses_train.append(np.asarray(loss).mean())
+                            results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, criterion_intents, model, lang)
+                            losses_dev.append(np.asarray(loss_dev).mean())
+                            f1.append(results_dev['total']['f'])
+                            accuracy.append(intent_res['accuracy'])
 
-    # PATH = os.path.join("bin", model_name)
-    # saving_object = {"epoch": x, 
-    #                  "model": model.state_dict(), 
-    #                  "optimizer": optimizer.state_dict(), 
-    #                  "w2id": w2id, 
-    #                  "slot2id": slot2id, 
-    #                  "intent2id": intent2id}
-    # torch.save(saving_object, PATH)
+                            if f1[-1] > best_f1:
+                                best_f1 = f1[-1]
+                                patience = 3
+                            else:
+                                patience -= 1
+                            if patience <= 0:
+                                break
 
+                    results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, criterion_intents, model, lang)    
+                    print('Slot F1: ', results_test['total']['f'])
+                    print('Intent Accuracy:', intent_test['accuracy'])
 
-    #PLOT OF THE TRAIN AND VALID LOSSES
-    plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
-    plt.title('Train and Dev Losses')
-    plt.ylabel('Loss')
-    plt.xlabel('Epochs')
-    plt.plot(sampled_epochs, losses_train, label='Train loss')
-    plt.plot(sampled_epochs, losses_dev, label='Dev loss')
-    plt.legend()
-    plt.show()
+                    all_results.append({
+                            'Batch Size': bs,
+                            'Learning Rate': lr,
+                            'Hid size': hid_size,
+                            'Dropout': d,
+                            'F1 score dev': max(f1),
+                            'Accuracy dev': max(accuracy)
+                        })
 
+                    results_df = pd.DataFrame({
+                        'Epoch': sampled_epochs,
+                        'F1 dev': f1,
+                        'Acc dev': accuracy,
+                        'F1 test': [results_test['total']['f']] * len(sampled_epochs),
+                        'Acc test': [intent_test['accuracy']] * len(sampled_epochs)
+                    })
+                    csv_filename = f'results/BERT_drop/BERT_drop_lr_{lr}_bs_{bs}_hid_{hid_size}_dropout_{d}.csv'
+                    results_df.to_csv(csv_filename, index=False)
+                    print(f'CSV file successfully saved in {csv_filename}')
 
-    #REMEMBER TO RUN THE BEST MODEL MULTIPLE TIMES
+                    plt.figure(num=3, figsize=(8, 5)).patch.set_facecolor('white')
+                    plt.title('Accuracy and F1 Score on Dev Set')
+                    plt.ylabel('Accuracy / F1 Score')
+                    plt.xlabel('Epochs')
+                    plt.plot(sampled_epochs, f1, label='F1 Score on Dev Set')
+                    plt.plot(sampled_epochs, accuracy, label='Accuracy on Dev Set')
+                    plt.legend()
+                    plt.xlim(min(sampled_epochs), max(sampled_epochs))
+                    plt.ylim(0.0, 1.0)
+                    res_plot_filename = f'results/BERT_drop/plots/BERT_res_plot_lr_{lr}_bs_{bs}_hid_{hid_size}_dropout_{d}.png'
+                    plt.savefig(res_plot_filename)
+                    print(f"F1 and Accuracy plot saved: '{res_plot_filename}'")
+                    plt.close()
+
+                    plt.figure(num=3, figsize=(8, 5)).patch.set_facecolor('white')
+                    plt.title('Train and Dev Losses')
+                    plt.ylabel('Loss')
+                    plt.xlabel('Epochs')
+                    plt.plot(sampled_epochs, losses_train, label='Train loss')
+                    plt.plot(sampled_epochs, losses_dev, label='Dev loss')
+                    plt.legend()
+                    plt.xlim(min(sampled_epochs), max(sampled_epochs))
+                    plt.ylim(0.0, 2.5)
+                    loss_plot_filename = f'results/BERT_drop/plots/BERT_loss_plot_lr_{lr}_bs_{bs}_hid_{hid_size}_dropout_{d}.png'
+                    plt.savefig(loss_plot_filename)
+                    print(f"Loss plot saved: '{loss_plot_filename}'")
+                    plt.close()
+
+                    print(f"Ending run #{current_configuration + 1} of {total_configurations}")
+                    print("=" * 89)
+                    current_configuration += 1
+                    print("=" * 89)
+
+    best_result_f1 = max(all_results, key=lambda x: x['F1 score dev'])
+    best_result_acc = max(all_results, key=lambda x: x['Accuracy dev'])
+    print(f"Best configuration f1: {best_result_f1}")
+    print(f"Best configuration acc: {best_result_acc}")
+    best_result_df_f1 = pd.DataFrame([best_result_f1])
+    best_result_df_acc = pd.DataFrame([best_result_acc])
+    best_result_df_f1.to_csv('results/BERT_drop/best_configuration_f1.csv', index=False)
+    best_result_df_acc.to_csv('results/BERT_drop/best_configuration_acc.csv', index=False)
+    print(f'Best configuration successfully saved')
--- 256181_Alessia_Ianes/NLU/part_B/model.py
+++ 256181_Alessia_Ianes/NLU/part_B/model.py
@@ -1,73 +1,24 @@
 import torch.nn as nn
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
-import torch
+from transformers import BertModel
 
-class ModelIAS(nn.Module):
+class BertModelIAS(nn.Module):
+    def __init__(self, hid_size, out_slot, out_int, dropout=0.1):
+        super(BertModelIAS, self).__init__()
+        self.bert = BertModel.from_pretrained('bert-base-uncased')
+        self.dropout = nn.Dropout(dropout)
+        self.slot_out = nn.Linear(self.bert.config.hidden_size, out_slot)
+        self.intent_out = nn.Linear(self.bert.config.hidden_size, out_int)
 
-    def __init__(self, hid_size, out_slot, out_int, emb_size, vocab_len, n_layer=1, pad_index=0):
-        super(ModelIAS, self).__init__()
-        # hid_size = Hidden size
-        # out_slot = number of slots (output size for slot filling)
-        # out_int = number of intents (output size for intent class)
-        # emb_size = word embedding size
-        
-        self.embedding = nn.Embedding(vocab_len, emb_size, padding_idx=pad_index)
-        
-        self.utt_encoder = nn.LSTM(emb_size, hid_size, n_layer, bidirectional=True, batch_first=True)   
+    def forward(self, utterance, attention_mask):
+        outputs = self.bert(input_ids=utterance, attention_mask=attention_mask)
+        sequence_output = outputs.last_hidden_state
+        pooled_output = outputs.pooler_output
 
-        # Adjust dimension of input for linear layers
-        # Since bidirectional LSTM output is 2 * hid_size,
-        # linear layers that recieves this output should have a corresponding input size.
- 
-        self.slot_out = nn.Linear(hid_size * 2, out_slot)
-        self.intent_out = nn.Linear(hid_size * 2, out_int)
-        # Dropout layer How/Where do we apply it?
-        self.dropout = nn.Dropout(0.1)
-        
-    def forward(self, utterance, seq_lengths):
-        # utterance.size() = batch_size X seq_len
-        utt_emb = self.embedding(utterance) # utt_emb.size() = batch_size X seq_len X emb_size
+        sequence_output = self.dropout(sequence_output)
+        pooled_output = self.dropout(pooled_output)
 
-        # Apply dropout to embeddings
-        # Apply dropout here helps to prevent overfitting on the embeddings themselves.
-        utt_emb = self.dropout(utt_emb) # utt_emb.size() = batch_size X seq_len X emb_size
-        
-        # pack_padded_sequence avoid computation over pad tokens reducing the computational cost
-        
-        packed_input = pack_padded_sequence(utt_emb, seq_lengths.cpu().numpy(), batch_first=True)
-        # Process the batch
-        packed_output, (last_hidden, cell) = self.utt_encoder(packed_input) 
-       
-        # Unpack the sequence
-        utt_encoded, input_sizes = pad_packed_sequence(packed_output, batch_first=True)
+        slots = self.slot_out(sequence_output)
+        intent = self.intent_out(pooled_output)
 
-        # Apply dropout to the output of the LSTM
-        # This is a common and effective place to apply dropout in RNNs,
-        # regularizing the output before it is passed to the slot classifier.
-        utt_encoded = self.dropout(utt_encoded)
-
-        # Get the last hidden state
-        # Obtain the correct last hidden state for a bidirectional LSTM
-        # For a bidirectional LSTM, `last_hidden` has the shape (num_layers * num_directions, batch_size, hidden_size).
-        # For a single layer (n_layer=1) bidirectional LSTM, the final states are:
-        # last_hidden[0,:,:] for the forward direction of the last layer
-        # last_hidden[1,:,:] for the backward direction of the last layer
-        # We need to concatenate these two to obtain a single vector that represents the entire utterance for the intent.
-        last_hidden =  torch.cat((last_hidden[-2,:,:], last_hidden[-1,:,:]), dim=1)
-
-        # Apply dropout to the last hidden state
-        # This is done to regularize the final representation before it is passed to the intent classifier..
-        last_hidden = self.dropout(last_hidden)
-
-        # Is this another possible way to get the last hiddent state? (Why?)
-        # utt_encoded.permute(1,0,2)[-1]
-        
-        # Compute slot logits
-        slots = self.slot_out(utt_encoded)
-        # Compute intent logits
-        intent = self.intent_out(last_hidden)
-        
-        # Slot size: batch_size, seq_len, classes 
-        slots = slots.permute(0,2,1) # We need this for computing the loss
-        # Slot size: batch_size, classes, seq_len
+        slots = slots.permute(0, 2, 1)
         return slots, intent
