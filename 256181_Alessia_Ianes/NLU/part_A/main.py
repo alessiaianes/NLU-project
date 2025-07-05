3fbb4fdcbafa6c092d764fcd6ef28ba20b4e8b95
alessiaianes <ianesalessia2@gmail.com>
2025-07-05 15:32:05 +0200

    Add .gitignore and new result CSV files for LSTM models; update model architecture for slot and intent outputs

--- 256181_Alessia_Ianes/NLU/part_A/main.py
+++ 256181_Alessia_Ianes/NLU/part_A/main.py
@@ -47,19 +47,6 @@ if __name__ == "__main__":
 
     y_test = [x['intent'] for x in test_raw]
 
-    # Intent distributions
-    # print('Train:')
-    # pprint({k:round(v/len(y_train),3)*100 for k, v in sorted(Counter(y_train).items())})
-    # print('Dev:'), 
-    # pprint({k:round(v/len(y_dev),3)*100 for k, v in sorted(Counter(y_dev).items())})
-    # print('Test:') 
-    # pprint({k:round(v/len(y_test),3)*100 for k, v in sorted(Counter(y_test).items())})
-    # print('='*89)
-    # Dataset size
-    # print('TRAIN size:', len(train_raw))
-    # print('DEV size:', len(dev_raw))
-    # print('TEST size:', len(test_raw))
-
 
     words = sum([x['utterance'].split() for x in train_raw], []) # No set() since we want to compute 
                                                             # the cutoff
@@ -75,12 +62,11 @@ if __name__ == "__main__":
     dev_dataset = IntentsAndSlots(dev_raw, lang)
     test_dataset = IntentsAndSlots(test_raw, lang)
 
-    hid_size_values = [300, 200] # Hidden size
+    hid_size = 300 # Hidden size
     emb_size = 300
-    batch_size_values = [128, 64, 32] # Batch size
-    dropout_values = [0.1, 0.2, 0.3, 0.4] # Dropout values
-
-    lr_values = [0.0001, 0.0005, 0.0007, 0.0009, 0.001] # learning rate
+    bs = 128 # Batch size
+    d = [0.1, 0.2, 0.3, 0.4] # Dropout values
+    lr = 0.0003 # learning rate
     clip = 5 # Clip the gradient
 
     out_slot = len(lang.slot2id)
@@ -88,158 +74,140 @@ if __name__ == "__main__":
     vocab_len = len(lang.word2id)
 
     # Create a directory to save the results, if it doesn't exist
-    os.makedirs('results/LSTM_drop/plots', exist_ok=True)
-    #os.makedirs('bin/LSTM_dropout_ADAM', exist_ok=True)
-    all_results = [] # To store the results of each configuration
-    total_configurations = len(lr_values) * len(hid_size_values) * len(batch_size_values) * len(dropout_values)
-    current_configuration = 0
-
-    for hid_size in hid_size_values:
-        for bs in batch_size_values:
-            for d in dropout_values:
-                for lr in lr_values:
-                    print("=" * 89)
-                    print(f"Starting run #{current_configuration + 1} of {total_configurations}")
-                    print("=" * 89)
-                    print(f"Running configuration: lr={lr}, Hid Size={hid_size}, Batch Size={bs}, Dropout={d}")
-
-                    # Dataloader instantiations
-                    train_loader = DataLoader(train_dataset, batch_size=bs, collate_fn=collate_fn,  shuffle=True)
-                    dev_loader = DataLoader(dev_dataset, batch_size=bs//2, collate_fn=collate_fn)
-                    test_loader = DataLoader(test_dataset, batch_size=bs//2, collate_fn=collate_fn)
-
-                    model = ModelIAS(hid_size, out_slot, out_int, emb_size, vocab_len, pad_index=PAD_TOKEN, dropout=d).to(device)
-                    model.apply(init_weights)
-
-                    optimizer = optim.Adam(model.parameters(), lr=lr)
-                    criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
-                    criterion_intents = nn.CrossEntropyLoss() # Because we do not have the pad token
-
-
-                    n_epochs = 200
-                    patience = 3
-                    losses_train = []
-                    losses_dev = []
-                    sampled_epochs = []
-                    f1 = []
-                    accuracy = []
-                    best_f1 = 0
-                    for x in tqdm(range(1,n_epochs)):
-                        loss = train_loop(train_loader, optimizer, criterion_slots, 
-                                        criterion_intents, model, clip=clip)
-                        if x % 5 == 0: # We check the performance every 5 epochs
-                            sampled_epochs.append(x)
-                            losses_train.append(np.asarray(loss).mean())
-                            results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
-                                                                        criterion_intents, model, lang)
-                            losses_dev.append(np.asarray(loss_dev).mean())
-                            
-                            
-                            f1.append(results_dev['total']['f'])
-                            accuracy.append(intent_res['accuracy'])
-                            # For decreasing the patience you can also use the average between slot f1 and intent accuracy
-                            if f1[-1] > best_f1:
-                                best_f1 = f1[-1]
-                                # Here you should save the model
-                                patience = 3
-                            else:
-                                patience -= 1
-                            if patience <= 0: # Early stopping with patience
-                                break # Not nice but it keeps the code clean
-
-                    
-
-                    results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, 
-                                                            criterion_intents, model, lang)    
-                    print('Slot F1: ', results_test['total']['f'])
-                    print('Intent Accuracy:', intent_test['accuracy'])
-
-                    all_results.append({
-                            'Batch Size': bs,
-                            'Learning Rate': lr,
-                            'Hid size': hid_size,
-                            'Dropout': d,
-                            'F1 score dev': max(f1),
-                            'Accuracy dev': max(accuracy)
-                        })
-
-
-                    # PATH = os.path.join("bin", model_name)
-                    # saving_object = {"epoch": x, 
-                    #                  "model": model.state_dict(), 
-                    #                  "optimizer": optimizer.state_dict(), 
-                    #                  "w2id": w2id, 
-                    #                  "slot2id": slot2id, 
-                    #                  "intent2id": intent2id}
-                    # torch.save(saving_object, PATH)
-
-                    # Save the results in a CSV file
-                    results_df = pd.DataFrame({
-                        'Epoch': sampled_epochs,
-                        'F1 dev': f1,
-                        'Acc dev': accuracy,
-                        'F1 test': [results_test['total']['f']] * len(sampled_epochs),
-                        'Acc test': [intent_test['accuracy']] * len(sampled_epochs)
-                    })
-                    csv_filename = f'results/LSTM_drop/LSTM_drop_lr_{lr}_bs_{bs}_hid_{hid_size}_dropout_{d}.csv'
-                    results_df.to_csv(csv_filename, index=False)
-                    print(f'CSV file successfully saved in {csv_filename}')
-
-
-                    # Create ppl_dev plot
-                    plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
-                    plt.title('Accuracy and F1 Score on Dev Set')
-                    plt.ylabel('Accuracy / F1 Score')
-                    plt.xlabel('Epochs')
-                    plt.plot(sampled_epochs, f1, label='F1 Score on Dev Set')
-                    plt.plot(sampled_epochs, accuracy, label='Accuracy on Dev Set')
-                    plt.legend()
-                    plt.xlim(min(sampled_epochs), max(sampled_epochs))
-                    plt.ylim(0.0, 1.0)  #lim for accuracy/F1
-                    #plt.show()
-
-                    # Save ppl_dev plot
-                    res_plot_filename = f'results/LSTM_drop/plots/LSTM_res_plot_lr_{lr}_bs_{bs}_hid_{hid_size}_dropout_{d}.png'
-                    plt.savefig(res_plot_filename)
-                    print(f"F1 and Accuracy plot saved: '{res_plot_filename}'")
-                    plt.close()
-
+    os.makedirs('results/LSTM_baseline/plots', exist_ok=True)
+    model_save_dir = "bin/LSTM_baseline"
+    os.makedirs(model_save_dir, exist_ok=True)
+    #all_results = [] # To store the results of each configuration
+    runs = 5
+    f1s, accs = [], []
     
 
+    # Dataloader instantiations
+    train_loader = DataLoader(train_dataset, batch_size=bs, collate_fn=collate_fn,  shuffle=True)
+    dev_loader = DataLoader(dev_dataset, batch_size=bs//2, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=bs//2, collate_fn=collate_fn)
 
-                    #PLOT OF THE TRAIN AND VALID LOSSES
-                    plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
-                    plt.title('Train and Dev Losses')
-                    plt.ylabel('Loss')
-                    plt.xlabel('Epochs')
-                    plt.plot(sampled_epochs, losses_train, label='Train loss')
-                    plt.plot(sampled_epochs, losses_dev, label='Dev loss')
-                    plt.legend()
-                    plt.xlim(min(sampled_epochs), max(sampled_epochs))
-                    plt.ylim(0.0, 2.5)  #lim for loss
-                    #plt.show()
+    for run in range(0, runs):
 
-                    # Save ppl_dev plot
-                    loss_plot_filename = f'results/LSTM_drop/plots/LSTM_loss_plot_lr_{lr}_bs_{bs}_hid_{hid_size}_dropout_{d}.png'
-                    plt.savefig(loss_plot_filename)
-                    print(f"Loss plot saved: '{loss_plot_filename}'")
-                    plt.close()
+        model = ModelIASBase(hid_size, out_slot, out_int, emb_size, vocab_len, pad_index=PAD_TOKEN).to(device)
+        model.apply(init_weights)
 
-                    print(f"Ending run #{current_configuration + 1} of {total_configurations}")
-                    print("=" * 89)
-                    current_configuration += 1
-                    print("=" * 89)
-
-    # After the loops, find the best configuration:
-    best_result_f1 = max(all_results, key=lambda x: x['F1 score dev'])
-    best_result_acc = max(all_results, key=lambda x: x['Accuracy dev'])
-    print(f"Best configuration f1: {best_result_f1}")
-    print(f"Best configuration acc: {best_result_acc}")
-    best_result_df_f1 = pd.DataFrame([best_result_f1])
-    best_result_df_acc = pd.DataFrame([best_result_acc])
-    best_result_df_f1.to_csv('results/LSTM_drop/best_configuration_f1.csv', index=False)
-    best_result_df_acc.to_csv('results/LSTM_drop/best_configuration_acc.csv', index=False)
-    print(f'Best configuration successfully saved')
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+        criterion_slots = nn.CrossEntropyLoss(ignore_index=PAD_TOKEN)
+        criterion_intents = nn.CrossEntropyLoss() # Because we do not have the pad token
 
 
-    #REMEMBER TO RUN THE BEST MODEL MULTIPLE TIMES
+        n_epochs = 200
+        patience = 3
+        losses_train = []
+        losses_dev = []
+        sampled_epochs = []
+        f1 = []
+        accuracy = []
+        best_f1 = 0
+        for x in tqdm(range(1,n_epochs)):
+            loss = train_loop(train_loader, optimizer, criterion_slots, 
+                            criterion_intents, model, clip=clip)
+            if x % 5 == 0: # We check the performance every 5 epochs
+                sampled_epochs.append(x)
+                losses_train.append(np.asarray(loss).mean())
+                results_dev, intent_res, loss_dev = eval_loop(dev_loader, criterion_slots, 
+                                                            criterion_intents, model, lang)
+                losses_dev.append(np.asarray(loss_dev).mean())
+                
+                
+                f1.append(results_dev['total']['f'])
+                accuracy.append(intent_res['accuracy'])
+                # For decreasing the patience you can also use the average between slot f1 and intent accuracy
+                if f1[-1] > best_f1:
+                    best_f1 = f1[-1]
+                    # Here you should save the model
+                    patience = 3
+                else:
+                    patience -= 1
+                if patience <= 0: # Early stopping with patience
+                    break # Not nice but it keeps the code clean
+
+        results_test, intent_test, _ = eval_loop(test_loader, criterion_slots, criterion_intents, model, lang)
+        accs.append(intent_test['accuracy'])
+        f1s.append(results_test['total']['f'])
+
+    f1s = np.asarray(f1s)
+    accs = np.asarray(accs)
+    print('Slot F1', round(f1s.mean(),3), '+-', round(f1s.std(),3))
+    print('Intent Acc', round(accs.mean(), 3), '+-', round(accs.std(), 3))
+
+    # Save detailed results per configuration to CSV
+    results_df = pd.DataFrame({
+        'Epoch': sampled_epochs,
+        'F1 dev': f1,
+        'Acc dev': accuracy,
+        'Loss Train': losses_train,
+        'Loss Dev': losses_dev,
+        # Add test scores repeated for each epoch line
+        'F1 test': [round(f1s.mean(),3)] * len(sampled_epochs),
+        'Acc test': [round(accs.mean(), 3)] * len(sampled_epochs)
+    })
+
+    csv_filename = f'results/LSTM_baseline/LSTM_baseline_lr_{lr}_bs_{bs}_hid_{hid_size}.csv'
+    results_df.to_csv(csv_filename, index=False)
+    print(f'CSV file successfully saved in {csv_filename}')
+
+    # Create ppl_dev plot
+    plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
+    plt.title('Accuracy and F1 Score on Dev Set')
+    plt.ylabel('Accuracy / F1 Score')
+    plt.xlabel('Epochs')
+    plt.plot(sampled_epochs, f1, label='F1 Score on Dev Set')
+    plt.plot(sampled_epochs, accuracy, label='Accuracy on Dev Set')
+    plt.legend()
+    plt.xlim(min(sampled_epochs), max(sampled_epochs))
+    plt.ylim(0.0, 1.0)  #lim for accuracy/F1
+    #plt.show()
+
+    # Save ppl_dev plot
+    res_plot_filename = f'results/LSTM_baseline/plots/LSTM_res_plot_lr_{lr}_bs_{bs}_hid_{hid_size}.png'
+    plt.savefig(res_plot_filename)
+    print(f"F1 and Accuracy plot saved: '{res_plot_filename}'")
+    plt.close()
+
+
+    #PLOT OF THE TRAIN AND VALID LOSSES
+    plt.figure(num = 3, figsize=(8, 5)).patch.set_facecolor('white')
+    plt.title('Train and Dev Losses')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.plot(sampled_epochs, losses_train, label='Train loss')
+    plt.plot(sampled_epochs, losses_dev, label='Dev loss')
+    plt.legend()
+    plt.xlim(min(sampled_epochs), max(sampled_epochs))
+    plt.ylim(0.0, 2.5)  #lim for loss
+    #plt.show()
+
+    # Save ppl_dev plot
+    loss_plot_filename = f'results/LSTM_baseline/plots/LSTM_loss_plot_lr_{lr}_bs_{bs}_hid_{hid_size}.png'
+    plt.savefig(loss_plot_filename)
+    print(f"Loss plot saved: '{loss_plot_filename}'")
+    plt.close()
+
+    config_params = {
+                    'Model': 'LSTM baseline', # Explicitly state the model used
+                    'Embedding Size': emb_size,
+                    'Hidden Size': hid_size,
+                    'Batch Size': bs,
+                    'Learning Rate': lr,
+                    #'Dropout': d
+                }
+
+    model_filename = f"LSTM_baseline_model.pt"
+    model_save_path = os.path.join(model_save_dir, model_filename)
+    saving_object = {
+        "epoch": x,
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+        "slot2id": lang.slot2id,
+        "intent2id": lang.intent2id,
+        "config": config_params, # Save the config that led to this best score
+    }
+    torch.save(saving_object, model_save_path)
+    print("model correctly saved")
